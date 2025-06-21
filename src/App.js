9facb020f14eca7e28f8fe578f0632b01d1a4ce6
Satoriga <eduardotorresj1526@gmail.com>
2025-06-20 22:29:42 -0500

    Registro completado

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Login from './Pages/Login/Login';
 import Store from './Pages/Store/Store';
 import Progress from './Pages/Progress/Progress';
+import Register from './Pages/Register/Register';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path='/Store' element = {<Store/>} />
           <Route path='/Progress' element = {<Progress/>} />
           <Route path='/Login' element = {<Login/>} />
+          <Route path='/Register' element = {<Register/>} />
         </Routes>
 
       </BrowserRouter>
