b3ea73577f6c5eb68eeeeea431393c1c6f13b838
Débora Martínez <mmartinezavalos1602@gmail.com>
2025-06-24 22:08:15 -0600

    HomePage final

--- src/Pages/HomePage/homepage.js
+++ src/Pages/HomePage/homepage.js
@@ -1,3 +1,22 @@
-document.querySelector('.hero-text button').addEventListener('click', () => {
-  alert('Let’s get started!');
-});
+import React from "react";
+import plateImage from "./imagenes1/plato_home.png"; // Asegúrate de poner tu imagen aquí
+
+function Home() {
+  return (
+    <div className="container-home">
+      <div className="text-section">
+        <h1>
+          Full of yum <br /> Full of life
+        </h1>
+        <button className="start-button">Let’s get started</button>
+      </div>
+
+      <div className="image-section">
+        <div className="green-background"></div>
+        <img src={plateImage} alt="Healthy Plate" className="plate-image" />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
